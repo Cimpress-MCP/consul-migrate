e486b619f28444661bdb770a460f8913f73dfa68
Calvin Leung Huang <cleung2010@gmail.com>
2015-09-08 16:14:48 -0400

    Add rough version of the CLI

--- lib/consul/migrate/cli.rb
+++ lib/consul/migrate/cli.rb
@@ -1,9 +1,28 @@
+require 'consul/migrate/version'
+require 'consul/migrate/client'
 require 'thor'
 
 module Consul
   module Migrate
     class Cli < Thor
 
+      attr_accessor :client
+
+      desc 'version', 'Display consul-migrate version'
+      def version
+        say "#{VERSION}"
+      end
+
+      desc 'init', 'Initialize consul-migrate instance'
+      option 'bind_client', type: :string, aliases: :c,
+                            desc: 'HTTPS client to bind to'
+      option 'port', type: :numeric, aliases: :p,
+                     desc: 'Port to bind to'
+      option 'acl_token', type: :string, aliases: :t,
+                          desc: 'ACL token that is used to access API'
+      def init
+        @client = Consul::Migrate::Client.new(options)
+      end
     end
   end
 end
