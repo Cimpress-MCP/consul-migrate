d2d8360231cb282280188ab89acc48ec23e53b9b
Calvin Leung Huang <cleung2010@gmail.com>
2015-09-09 12:20:09 -0400

    Use init.conf to store client settings

--- lib/consul/migrate/cli.rb
+++ lib/consul/migrate/cli.rb
@@ -1,6 +1,8 @@
 require 'consul/migrate/version'
 require 'consul/migrate/client'
 require 'thor'
+require 'yaml'
+require 'fileutils'
 
 module Consul
   module Migrate
@@ -8,20 +10,63 @@ module Consul
 
       attr_accessor :client
 
+      def initialize(*args)
+        super
+        read_config
+      end
+
       desc 'version', 'Display consul-migrate version'
       def version
         say "#{VERSION}"
       end
 
       desc 'init', 'Initialize consul-migrate instance'
-      option 'bind_client', type: :string, aliases: :c,
-                            desc: 'HTTPS client to bind to'
-      option 'port', type: :numeric, aliases: :p,
-                     desc: 'Port to bind to'
-      option 'acl_token', type: :string, aliases: :t,
-                          desc: 'ACL token that is used to access API'
+      option :bind_client, type: :string, aliases: :c,
+                           desc: 'HTTPS client to bind to'
+      option :port, type: :numeric, aliases: :p,
+                    desc: 'Port to bind to'
+      option :acl_token, type: :string, aliases: :t,
+                         desc: 'ACL token that is used to access API'
       def init
-        @client = Consul::Migrate::Client.new(options)
+        write_config(options)
+      end
+
+      desc 'export', 'Export ACLs from Consul via agent running in this system'
+      option :file, type: :string, default: 'output.json', aliases: :f,
+                     desc: 'Target file to write to'
+      def export
+        @client.export_acls(options[:file])
+      end
+
+      desc 'import', 'Import ACLs into Consul cluster via agent running in this system'
+      option :file, type: :string, default: 'output.json', aliases: :f,
+                     desc: 'Target file to read from'
+      def import
+        @client.import_acls(options[:file])
+      end
+
+      private
+
+      CONFIG_FILE = File.expand_path('~/.cmigrate/init.conf')
+
+      def write_config(options)
+        parent_dir = File.dirname(CONFIG_FILE)
+        FileUtils.mkdir_p parent_dir
+
+        client = Consul::Migrate::Client.new(options)
+        new_options = client.options
+
+        File.open(CONFIG_FILE, 'w') do |f|
+          # Convert keys from symbols to string and turn hash into YAML
+          f.write Hash[new_options.map{|(k,v)| [k.to_s,v]}].to_yaml
+        end
+      end
+
+      def read_config
+        return unless File.exists?(CONFIG_FILE)
+
+        config = YAML.load_file(CONFIG_FILE)
+        @client = Consul::Migrate::Client.new(config)
       end
     end
   end
